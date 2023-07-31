32ede5edb242dccfe27f7d3f421f34f8d4cafa92
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-07-31 06:18:41 +0000

    added more css and added a toogle button

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,11 +1,44 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const TrafficLight = () =>{
 
     const [chosenColor , setChosenColor] = useState ("")
-    const [toggleLight , setToggleLight] = useState()
+    const [toggleLight , setToggleLight] = useState(false)
+
+    useEffect (() =>{
+        let interval;
+
+        if(toggleLight){
+            interval = setInterval(() =>{
+                setChosenColor((prevLight) => {
+                    if(prevLight === "red"){
+                        return "green";
+                    }
+                    else if (prevLight === "green"){
+                        return "yellow";
+                    }
+                    else{
+                        return "red";
+                    }    
+                });
+              
+            },3000);
+
+        }
+        return () =>{
+            clearInterval(interval);
+        };
+
+    },[toggleLight]);
+              
+    const toggleButton = () =>{
+        setToggleLight((prevToggle) => !prevToggle);
+
+    }
+
     return (
         <>
+
             <div className="traffic-Light-stem"></div>
             <div className="traffic-light-body">
                 <div 
@@ -25,6 +58,10 @@ const TrafficLight = () =>{
                 
                 ></div>
 
+            </div>
+           
+            <div className="button">
+                <button className="btn btn-info mt-3" onClick={toggleButton}>Click To Toggle</button>
             </div>
 
         </>
